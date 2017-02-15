a700464bc1065f24c236cf4868d68758f0b218c2
omgirok <ouorion@gmail.com>
2017-02-15 12:40:38 -0800

    update print statements

--- tweetbot_songoftheday.py
+++ tweetbot_songoftheday.py
@@ -3,20 +3,28 @@ import tweepy
 from time import sleep
 import xml.etree.ElementTree as ET
 import random
+import datetime
+
 
 # Set up OAuth and integrate with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-
+# parse music library
 tree = ET.parse('Library.xml')
 root = tree.getroot()
-
+# Move cursor to 'Songs'
 nav = root.find('dict')
 nav = nav.find('dict')
 
 trackDict = nav.findall('dict')
+# Shuffle for song of the day
 random.shuffle(trackDict)
+
+
+today = datetime.date.today()
+
+
 for track in trackDict:
 	try:
 		if track[4].text != 'Artist':
@@ -24,6 +32,7 @@ for track in trackDict:
 		else:
 			trackArtist = track[5].text
 			trackName = track[3].text
+			print "Song of the day " + date.strfmtime(today, '%A, %B %d %Y') 
 			print trackName + " by " + trackArtist
 			api.update_status(status="Today's song of the day is " + trackName + " by " + trackArtist + "! Hope you enjoy :)") 
 			sleep(60*60*24)
